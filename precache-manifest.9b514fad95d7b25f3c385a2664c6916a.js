b29d1c4a10b52416dd57aa6f018a1a1386a6f562
Bhavyajot Malhotra <malhotrabhavyajot@gmail.com>
2020-11-12 19:43:57 +0530

    Updates

---
+++ precache-manifest.9b514fad95d7b25f3c385a2664c6916a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "114ec369f103345c8cd38863e55a505f",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "074204139b21ecbdba19",
+    "url": "/React/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "e783cc1d13d6b5c0cf69",
+    "url": "/React/static/css/main.7b868e90.chunk.css"
+  },
+  {
+    "revision": "074204139b21ecbdba19",
+    "url": "/React/static/js/2.debc1488.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React/static/js/2.debc1488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e783cc1d13d6b5c0cf69",
+    "url": "/React/static/js/main.7093c8bc.chunk.js"
+  },
+  {
+    "revision": "a0c79aae7275c29e2a44",
+    "url": "/React/static/js/runtime-main.37c9e924.js"
+  }
+]);
